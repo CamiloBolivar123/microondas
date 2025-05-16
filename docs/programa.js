0d8ebf490ebbe0c07de782e0176c2c123d302beb
Brayan Bolivar <brayanbolivar323@gmail.com>
2025-05-16 16:02:15 -0500

    V 0.6

--- docs/programa.js
+++ docs/programa.js
@@ -1,4 +1,4 @@
-const mapa = L.map('mapa').setView([4.628178084725448, -74.0659272612135], 14);
+var mapa = L.map('mapa').setView([4.628178084725448, -74.0659272612135], 14);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
@@ -19,26 +19,13 @@ async function cargarPuntos() {
         for (let i = 0; i < listaFeatures.length; i++) {
             const feature = listaFeatures[i];
 
-            if (
-                feature &&
-                feature.geometry &&
-                Array.isArray(feature.geometry.coordinates) &&
-                feature.geometry.coordinates.length === 2
-            ) {
                 const coords = feature.geometry.coordinates;
-                const latLng = [coords[1], coords[0]];
-
                 const propiedades = feature.properties;
+                const marcador = L.marker(coords).addTo(mapa);
 
-                const marcador = L.marker(latLng).addTo(mapa);
+                console.log(marcador);
 
-                console.log(`Punto ${i + 1}`);
-                console.log("Modelo:", propiedades.modelo);
-                console.log("Capacidad:", propiedades.capacidad);
-                console.log("Precio:", propiedades.precio);
-                console.log("Potencia:", propiedades.potencia);
-                console.log("Voltaje:", propiedades.voltaje);
-                console.log("---------------");
+                
 
                 const contenidoPopup = `
                     <strong>Modelo:</strong> ${propiedades.modelo}<br>
@@ -49,9 +36,7 @@ async function cargarPuntos() {
                 `;
 
                 marcador.bindPopup(contenidoPopup);
-            } else {
-                console.warn(`Feature ${i} sin coordenadas válidas.`);
-            }
+          
         }
     } catch (error) {
         console.error("Error al cargar el archivo GeoJSON:", error);
